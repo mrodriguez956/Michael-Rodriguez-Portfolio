6b6290cbdf13502edf198a3175a7761c90962f00
Michael Rodriguez <apple.mrodriguez956@gmail.com>
2024-04-26 00:38:11 -0500

    addded animation for hover on cards

--- script.js
+++ script.js
@@ -9,3 +9,41 @@ function onScroll() {
     document.body.classList.add("scroll-top");
   }
 }
+
+function toggleAnimation()
+{
+  const containers = document.getElementsByClassName("card");
+  const captionTexts = document.getElementsByClassName("caption-text");
+
+  for (let i = 0; i < containers.length; i++)
+  {
+    const container = containers[i];
+    const captionText = captionTexts[i];
+
+    container.addEventListener("mouseenter", function(event)
+    {
+      if(event.currentTarget === container)
+      {
+        captionText.classList.remove("animate__bounceOut");
+        captionText.classList.add("animate__bounceIn");
+      }
+    });
+
+    container.addEventListener("mouseleave", function(event)
+    {
+      if(event.currentTarget === container)
+      {
+        captionText.classList.remove("animate__bounceIn");
+        captionText.classList.add("animate__bounceOut");
+        
+      }
+    });
+    
+  }
+
+
+}
+
+
+window.onload = toggleAnimation();
+
